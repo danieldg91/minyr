1a90f431c929b6c16a7e74b2b81e6f368e71dd19
danieldahlegray <danieldahlegray@gmail.com>
2023-04-06 11:59:57 +0200

    fixing package import.2

--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
-module github.com/uia-worker/minyr
+module github.com/danieldg91/minyr
 
-go 1.18
+go 1.19
 
-require github.com/uia-worker/misc v0.0.0-20230306201706-92c01d4aa553
+require github.com/danieldg91/funtemps v0.0.0-20230406094022-1d786f667fdc
--- main.go
+++ main.go
@@ -1,27 +1,27 @@
 package main
 
 import (
-	"os"
-	"log"
 	"io"
+	"log"
+	"os"
 	"strings"
-	"github.com/uia-worker/misc/conv"
-)	
+
+	"github.com/danieldg91/funtemps/conv"
+)
 
 func main() {
 	src, err := os.Open("table.csv")
 	//src, err := os.Open("/home/janisg/minyr/kjevik-temp-celsius-20220318-20230318.csv")
 	if err != nil {
-        	log.Fatal(err)
+		log.Fatal(err)
 	}
 	defer src.Close()
-        log.Println(src)
-        
-	
+	log.Println(src)
+
 	var buffer []byte
 	var linebuf []byte // nil
 	buffer = make([]byte, 1)
-        bytesCount := 0
+	bytesCount := 0
 	for {
 		_, err := src.Read(buffer)
 		if err != nil && err != io.EOF {
@@ -31,18 +31,18 @@ func main() {
 		bytesCount++
 		//log.Printf("%c ", buffer[:n])
 		if buffer[0] == 0x0A {
-	           log.Println(string(linebuf))
-		   // Her
-		   elementArray := strings.Split(string(linebuf), ";")
-		   if len(elementArray) > 3 {
-			 celsius := elementArray[3]
-			 fahr := conv.CelsiusToFahrenheit(celsius)
-		         log.Println(elementArray[3])
-	   	   }
-                   linebuf = nil		   
+			log.Println(string(linebuf))
+			// Her
+			elementArray := strings.Split(string(linebuf), ";")
+			if len(elementArray) > 3 {
+				celsius := elementArray[3]
+				fahr := conv.CelsiusToFahrenheit(celsius)
+				log.Println(elementArray[3])
+			}
+			linebuf = nil
 		} else {
-                   linebuf = append(linebuf, buffer[0])
-		}	
+			linebuf = append(linebuf, buffer[0])
+		}
 		//log.Println(string(linebuf))
 		if err == io.EOF {
 			break
